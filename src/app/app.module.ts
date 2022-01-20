5a5e9a1a3b793d34adcf08d5f218911f0a83e693
Giorgi <grgpaikidze@gmail.com>
2022-01-20 11:13:48 +0400

    init: Add angular material package.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { CoreModule } from './core/core.module';
 import { PagesModule } from './pages/pages.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ComponentsModule, CoreModule, PagesModule],
+  imports: [BrowserModule, ComponentsModule, CoreModule, PagesModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
