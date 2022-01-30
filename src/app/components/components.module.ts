c09dd0a2fbcfc650a0632e01ff111c0a49212f6a
Giorgi <grgpaikidze@gmail.com>
2022-01-30 23:02:02 +0400

    feat: add input component.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
 import { ButtonComponent } from './button/button.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [
-    ButtonComponent
-  ],
-  imports: [
-    CommonModule,
-    MatIconModule
-  ],
-  exports: [
-    ButtonComponent
-  ]
+  declarations: [ButtonComponent, InputComponent],
+  imports: [CommonModule, MatIconModule],
+  exports: [ButtonComponent, InputComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
