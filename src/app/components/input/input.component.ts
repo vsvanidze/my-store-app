2cd80ea0d884f46d3d226d305dcdf06acca5c1e0
Giorgi <grgpaikidze@gmail.com>
2022-02-09 16:02:02 +0400

    feat: add ControlValueAccessor.

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,13 +1,33 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewEncapsulation,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormBuilder,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { InputType } from 'src/app/core/models/input-type';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: InputComponent,
+    },
+  ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor, OnDestroy {
   inputType = InputType;
 
   @Input()
@@ -20,13 +40,38 @@ export class InputComponent {
     input: '',
   });
 
+  onChangeSub!: Subscription;
+  onTouched = () => {};
+
   constructor(private fb: FormBuilder) {}
+
+  ngOnDestroy(): void {
+    this.onChangeSub.unsubscribe();
+  }
 
   onInputText(text: string) {
     this.inputEvent.emit(text);
   }
 
-  get inputValue() {
-    return this.inputForm.value.input;
+  writeValue(value: any) {
+    if (value) {
+      this.inputForm.setValue(value);
+    }
+  }
+
+  registerOnChange(onChange: any) {
+    this.onChangeSub = this.inputForm.valueChanges.subscribe(onChange);
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    if (isDisabled) {
+      this.inputForm.disable();
+    } else {
+      this.inputForm.enable();
+    }
   }
 }
